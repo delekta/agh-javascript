a0160ad6249f5c7c69c121f7644244c81cfea83f
Kamil Delekta <dkamil17@wp.pl>
2021-04-20 23:06:25 +0200

    lab3/zad3alt Web Components added

--- lab3/zad3alt.js
+++ lab3/zad3alt.js
@@ -1,4 +1,4 @@
-var spans = document.getElementsByTagName("span")
+var myspans = document.getElementsByTagName("my-span")
 var btn = document.getElementById("licznikBtn")
 var input = document.getElementById("licznik");
 var _default = 10; 
@@ -7,22 +7,52 @@ var currValue = _default
 
 btn.addEventListener('click', () => {
     currValue = input.value
-    for(let i = 0; i < spans.length; i++){
-        spans[i].textContent = String(currValue); 
+    for(let i = 0; i < myspans.length; i++){
+        updateMySpans()
     }
     setInterval(decrementSpans, 1000)
 })
 
-
-
 function decrementSpans(){
     currValue -= 1
-    for(let i = 0; i < spans.length; i++){
-        if(currValue >= 0){
-            spans[i].textContent = String(currValue);
-            if(currValue == 0){
-                input.value = 0
-            } 
-        }
+    updateMySpans()
+}
+
+function updateMySpans(){
+    for(let myspan of myspans){
+        myspan.value = currValue;
     }
 }
+
+class MySpan extends HTMLElement {
+    constructor() {
+        super();
+        // Attach a shadow root to the element.
+        let shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.innerHTML = `<span>0</span>`;
+    }
+    get value(){
+        return this.getAttribute("value");
+    }
+    set value(val){
+        this.setAttribute("value", val);
+    }
+
+    static get observedAttributes(){
+        return ["value"]
+    }
+
+    attributeChangedCallback(prop, oldVal, newVal){
+        if(prop === "value" && this.value >= 0){
+            this.render();
+        }
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+    render(){
+        this.shadowRoot.innerHTML=`${this.value}`;
+    }
+}
+customElements.define('my-span', MySpan);
