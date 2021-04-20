80d96c1071e5781e2959e2297e9e4b948f816421
Kamil Delekta <dkamil17@wp.pl>
2021-04-20 21:20:11 +0200

    Fixing lab3/zad3

--- lab3/zad3.js
+++ lab3/zad3.js
@@ -1,6 +1,7 @@
 var spans = document.getElementsByTagName("span")
 var btn = document.getElementById("licznikBtn")
 var input = document.getElementById("licznik");
+var wrapper = document.getElementById("wrapper");
 var _default = 10; 
 input.value = _default;
 var currValue = _default
@@ -9,19 +10,19 @@ btn.addEventListener('click', () => {
     currValue = input.value
     for(let i = 0; i < spans.length; i++){
         var newElement = document.createElement("span");
-        newElement.value = String(currValue)
-        spans[i].textContent = String(currValue); 
+        newElement.textContent = String(currValue)
+        wrapper.replaceChild(newElement, spans[i])
     }
     setInterval(decrementSpans, 1000)
 })
-
-
 
 function decrementSpans(){
     currValue -= 1
     for(let i = 0; i < spans.length; i++){
         if(currValue >= 0){
-            spans[i].textContent = String(currValue);
+            var newElement = document.createElement("span");
+            newElement.textContent = String(currValue)
+            wrapper.replaceChild(newElement, spans[i])
             if(currValue == 0){
                 input.value = 0
             } 
